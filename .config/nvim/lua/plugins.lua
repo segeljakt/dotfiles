e4edfcb2a6077c7fe3e12bccc0b3dc610c60f4e5
Klas Segeljakt <klasseg@kth.se>
2022-11-05 17:21:14 +0100

    Cleanup

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,10 +1,6 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
-
--- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+vim.cmd('packadd packer.nvim')
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
   use 'wbthomason/packer.nvim'
   use {
     'folke/twilight.nvim',
@@ -152,79 +148,6 @@ return require('packer').startup(function(use)
       }
     end
   }
-  --   use {
-  --     'stevearc/dressing.nvim',
-  --     config = function()
-  --       require('dressing').setup({
-  --         input = {
-  --           enabled = true,
-  --           default_prompt = "Input:",
-  --           prompt_align = "left",
-  --           insert_only = true,
-  --           anchor = "SW",
-  --           border = "rounded",
-  --           relative = "cursor",
-  --           prefer_width = 40,
-  --           width = nil,
-  --           max_width = { 140, 0.9 },
-  --           min_width = { 20, 0.2 },
-  --           winblend = 10,
-  --           winhighlight = "",
-  --           override = function(conf)
-  --             return conf
-  --           end,
-  --           get_config = nil,
-  --         },
-  --         select = {
-  --           enabled = true,
-  --           backend = { "telescope", "fzf_lua", "fzf", "builtin", "nui" },
-  --           telescope = nil,
-  --           fzf = {
-  --             window = {
-  --               width = 0.5,
-  --               height = 0.4,
-  --             },
-  --           },
-  --           fzf_lua = {
-  --             winopts = {
-  --               width = 0.5,
-  --               height = 0.4,
-  --             },
-  --           },
-  --           nui = {
-  --             position = "50%",
-  --             size = nil,
-  --             relative = "editor",
-  --             border = {
-  --               style = "rounded",
-  --             },
-  --             max_width = 80,
-  --             max_height = 40,
-  --           },
-  --           builtin = {
-  --             anchor = "NW",
-  --             border = "rounded",
-  --             relative = "editor",
-  --             winblend = 10,
-  --             winhighlight = "",
-  --             width = nil,
-  --             max_width = { 140, 0.8 },
-  --             min_width = { 40, 0.2 },
-  --             height = nil,
-  --             max_height = 0.9,
-  --             min_height = { 10, 0.2 },
-  --
-  --             override = function(conf)
-  --               return conf
-  --             end,
-  --           },
-  --           format_item_override = {},
-  --           get_config = nil,
-  --         },
-  --       })
-  --
-  --     end
-  --   }
   -- [Color-Schemes]
   use 'nlknguyen/papercolor-theme'
 
@@ -250,17 +173,6 @@ return require('packer').startup(function(use)
       alpha.setup(startify.config)
     end
   }
-  use {
-    'ldelossa/litee.nvim'
-  }
-  -- use {
-  --   'ldelossa/gh.nvim',
-  --   requires = { { 'ldelossa/litee.nvim' } },
-  --   config = function()
-  --     require('litee.lib').setup({})
-  --     require('litee.gh').setup({})
-  --   end
-  -- }
 
   -- [General-purpose]
   -- use 'josa42/vim-lightline-coc'
